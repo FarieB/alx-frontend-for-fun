2647573c7067410746077c543e5007fc289c7e3e
FarieB <faraibande@gmail.com>
2024-10-20 18:48:20 +0000

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import os
+
+# Check if the number of arguments is less than 2
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# Check if the input Markdown file exists
+if not os.path.isfile(input_file):
+    print(f"Missing {input_file}", file=sys.stderr)
+    sys.exit(1)
+
+# If no errors, exit with status 0 (successful execution)
+sys.exit(0)
